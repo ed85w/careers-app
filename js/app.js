5d7a80b7e85be9469e1b606d87c063dac016fe7d
ed85w <ed.walker@hotmail.co.uk>
2021-03-16 08:59:33 +0000

    branch switch

--- js/app.js
+++ js/app.js
@@ -2,94 +2,101 @@ var app = new Vue({
   el: '#app',
   data: {
     allQuestions: [
-      {"questionTitle": "What is your preferred working environment?",
+      {"questionTitle": "What’s your ideal work environment?",
       "questionClass": "inactive",
       "questionAnswerValue" : "",
       "questionAnswerText" : "",
       "options" : [
         { text: 'Office-based', value: '1' },
         { text: 'Laboratory/Scientific Facility', value: '2' },
-        { text: 'Out & About (e.g. field-work, travel, outdoors)', value: '3' }
+        { text: 'Out & About (e.g. working with external clients or customers or field-work)', value: '3' }
         ]
       },
-      {"questionTitle": "question 2",
+      {"questionTitle": "What size team do you prefer to work in?",
       "questionClass": "inactive",
       "questionAnswerValue" : "",
       "questionAnswerText" : "",
       "options" : [
-        { text: 'Question 2 option 1', value: '1' },
-        { text: 'Question 2 option 2', value: '2' },
-        { text: 'Question 2 option 3', value: '3' }
+        { text: 'Small team', value: '1' },
+        { text: 'Large team', value: '2' },
         ]
       },
-      {"questionTitle": "question 3",
+      {"questionTitle": "In your job, do you mostly want to use your...",
       "questionClass": "inactive",
       "questionAnswerValue" : "",
       "questionAnswerText" : "",
       "options" : [
-        { text: 'question 3 option 1', value: '1' },
-        { text: 'question 3 option 2', value: '2' },
-        { text: 'question 3 option 3', value: '3' }
+        { text: 'Physics Knowledge', value: '1' },
+        { text: 'Computing / Coding Skills (Mathematicsand data analysis?)', value: '2' },
+        { text: 'Something else', value: '3' }
         ]
       },
     ],
     careers: [
-      {"title": "job1",
-        "question1" : "1 2",
-        "question2" : "1 2",
-        "question3" : "1 2",
-        "information": [
-          "some information about job 1",
-          "some more information about job 1"
-        ]
-      },
-      {"title": "job2",
+      {"title": "Nuclear Engineer",
         "question1" : "1",
         "question2" : "2",
-        "question3" : "2",
+        "question3" : "1",
+        "information": [
+          "some information about Nuclear Engineer",
+          "some more information about Nuclear Engineer"
+        ]
+      },
+      {"title": "Metallurgist",
+        "question1" : "2",
+        "question2" : "1",
+        "question3" : "1",
         "information": [
           "job 2 would be a good job",
           "you would do lots of maths"
         ]
       },
-      {"title": "job3",
-        "question1" : "3",
-        "question2" : "2",
-        "question3" : "3",
+      {"title": "Applications Developer",
+        "question1" : "1",
+        "question2" : "1",
+        "question3" : "2",
         "information": [
           "hello",
           "hello again"
         ]
       },
-      {"title": "job4",
-        "question1" : "1 2 3",
-        "question2" : "1 2 3",
-        "question3" : "1 2 3",
+      {"title": "Data Analyst",
+        "question1" : "1",
+        "question2" : "1",
+        "question3" : "2",
         "information": [
           "job 4!",
           "job 4 involves physics so would appeal to someone who enjoys physics",
         ]
       },
-      {"title": "job5",
-        "question1" : "3",
-        "question2" : "1 3",
-        "question3" : "1 2 3",
+      {"title": "Financial Quantitative Analyst",
+      "question1" : "1",
+      "question2" : "1",
+      "question3" : "2",
         "information": [
           "job 5!",
         ]
       },
-      {"title": "job6",
-        "question1" : "1",
-        "question2" : "2",
-        "question3" : "3",
+      {"title": "Geophysicist",
+        "question1" : "3",
+        "question2" : "1",
+        "question3" : "1",
         "information": [
           "job 6!",
         ]
       },
-      {"title": "job7",
+      {"title": "Meteorologist",
+        "question1" : "2",
+        "question2" : "1",
+        "question3" : "1",
+        "information": [
+          "job 7!",
+        ]
+      },
+      {"title": "Royal Air Force Airperson",
         "question1" : "3",
-        "question2" : "2 3",
-        "question3" : "1 2 3",
+        "question2" : "2",
+        "question3" : "3",
         "information": [
           "job 7!",
         ]
@@ -124,7 +131,7 @@ var app = new Vue({
   },
   computed: {
     careerFilter: function () {
-      return this.careers.filter(career => career.question1.includes(this.allQuestions[0].questionAnswerValue) && career.question1.includes(this.allQuestions[1].questionAnswerValue) && career.question1.includes(this.allQuestions[2].questionAnswerValue));
+      return this.careers.filter(career => career.question1.includes(this.allQuestions[0].questionAnswerValue) && career.question2.includes(this.allQuestions[1].questionAnswerValue) && career.question3.includes(this.allQuestions[2].questionAnswerValue));
     }
   }
 })
