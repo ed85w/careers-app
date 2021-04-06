a5fb62969da6b328b8d6876a9e0b4dbeee7b8e25
ed85w <ed.walker@hotmail.co.uk>
2021-04-06 14:38:27 +0100

    deploy test

--- js/app.js
+++ js/app.js
@@ -1,83 +1,92 @@
 var app = new Vue({
   el: '#app',
   data: {
+    quizStarted: false,
+    quizClass: "inactive",
+    questionsContainerHeight: "",
     allQuestions: [
-      {"questionTitle": "What’s your ideal work environment?",
-      "questionClass": "inactive",
-      "questionAnswerValue" : "",
-      "questionAnswerText" : "",
-      "options" : [
-        { text: 'Office-based', value: '1' },
-        { text: 'Laboratory/Scientific Facility', value: '2' },
-        { text: 'Out & About (e.g. working with external clients or customers or field-work)', value: '3' }
-        ]
+      {
+        "questionHeader": "Working Environment",
+        "questionImg": "working-environment.jpg",
+        "questionImgAlt": "A picture of a man working at a desk",
+        "questionTitle": "What’s your ideal work environment?",
+        "questionClass": "inactive",
+        "questionAnswerValue" : "",
+        "questionAnswerText" : "",
+        "options" : [
+          { text: 'Office-based', value: '1' },
+          { text: 'Laboratory/Scientific Facility', value: '2' },
+          { text: 'Out & About (e.g. working with external clients or customers or field-work)', value: '3' }
+          ]
       },
-      {"questionTitle": "What size team do you prefer to work in?",
-      "questionClass": "inactive",
-      "questionAnswerValue" : "",
-      "questionAnswerText" : "",
-      "options" : [
-        { text: 'Small team', value: '1' },
-        { text: 'Large team', value: '2' },
-        ]
+      {
+        "questionHeader": "Team Size",
+        "questionImg": "team-size.jpg",
+        "questionImgAlt": "TBC",
+        "questionTitle": "What size team do you prefer to work in?",
+        "questionClass": "inactive",
+        "questionAnswerValue" : "",
+        "questionAnswerText" : "",
+        "options" : [
+          { text: 'Small team', value: '1' },
+          { text: 'Large team', value: '2' },
+          ]
       },
-      {"questionTitle": "In your job, do you mostly want to use your...",
-      "questionClass": "inactive",
-      "questionAnswerValue" : "",
-      "questionAnswerText" : "",
-      "options" : [
-        { text: 'Physics Knowledge', value: '1' },
-        { text: 'Computing / Coding Skills (Mathematicsand data analysis?)', value: '2' },
-        { text: 'Something else', value: '3' }
-        ]
-      },
+      {
+        "questionHeader": "Job Skills",
+        "questionImg": "team-size.jpg",
+        "questionImgAlt": "TBC",
+        "questionTitle": "In your job, do you mostly want to use your...",
+        "questionClass": "inactive",
+        "questionAnswerValue" : "",
+        "questionAnswerText" : "",
+        "options" : [
+          { text: 'Physics Knowledge', value: '1' },
+          { text: 'Computing / Coding Skills', value: '2' },
+          { text: 'Something else', value: '3' }
+          ]
+        },
     ],
     careers: [
       {"title": "Nuclear Engineer",
         "question1" : "1",
         "question2" : "2",
         "question3" : "1",
-        "information": [
-          "some information about Nuclear Engineer",
-          "some more information about Nuclear Engineer"
-        ],
-        "link": "https://docs.google.com/document/d/1-1eH7S-fRLX68XvpFSqB1y2XbrqJExqE1nXD-5nWSqQ"
+        "link": "https://docs.google.com/document/d/1-1eH7S-fRLX68XvpFSqB1y2XbrqJExqE1nXD-5nWSqQ",
+        "img": "",
+        "imgAlt": "TO ENTER"
       },
       {"title": "Metallurgist",
         "question1" : "2",
         "question2" : "1",
         "question3" : "1",
-        "information": [
-          "Metallurgist info",
-        ],
-        "link": "./metallurgist.html"
+        "link": "./metallurgist.html",
+        "img": "metallurgist.png",
+        "imgAlt": "TO ENTER"
       },
       {"title": "Applications Developer",
         "question1" : "1",
         "question2" : "1",
         "question3" : "2",
-        "information": [
-          "Applications Developer info",
-          "hello again"
-        ],
-        "link": "https://docs.google.com/document/d/1vrh-yDQVkBtyM5odgiVq3rlN0jwyZc0F6P846HBBtzg"
+        "link": "./applications-developer.html",
+        "img": "applications-developer.png",
+        "imgAlt": "image of a laptop"
       },
       {"title": "Data Analyst",
         "question1" : "1",
         "question2" : "1",
         "question3" : "2",
-        "information": [
-          "Data Analyst",
-          "job 4 involves physics so would appeal to someone who enjoys physics",
-        ]
+        "link": "./metallurgist.html",
+        "img": "./metallurgist.html",
+        "imgAlt": "TO ENTER"
       },
       {"title": "Financial Quantitative Analyst",
       "question1" : "1",
       "question2" : "1",
       "question3" : "2",
-        "information": [
-          "Financial Quantitative Analyst",
-        ]
+      "link": "./metallurgist.html",
+      "img": "./metallurgist.html",
+      "imgAlt": "TO ENTER"
       },
       {"title": "Geophysicist",
         "question1" : "3",
@@ -104,8 +113,6 @@ var app = new Vue({
         ]
       },
     ],
-    quizStarted: false,
-    quizClass: "inactive"
   },
   methods: {
     quizStart: function(){
@@ -113,8 +120,8 @@ var app = new Vue({
       this.quizStarted = true
     },
     nextQuestion: function(index){
-      this.allQuestions[index].questionClass = "done"
-      this.allQuestions[index + 1].questionClass = "active"
+      this.allQuestions[index].questionClass = "done";
+      this.allQuestions[index + 1].questionClass = "active";
     },
     prevQuestion: function(index) {
       // if 1st question 
@@ -129,23 +136,24 @@ var app = new Vue({
     // assigns the text version of each answer to the relevant question 
     setAnswerText: function(val, index) {
       this.allQuestions[index].questionAnswerText = val.srcElement.nextSibling.data
-    }
+    },
   },
   computed: {
     careerFilter: function () {
       return this.careers.filter(career => career.question1.includes(this.allQuestions[0].questionAnswerValue) && career.question2.includes(this.allQuestions[1].questionAnswerValue) && career.question3.includes(this.allQuestions[2].questionAnswerValue));
     },
-    // resetQuiz: function(){
-    //   if(!this.quizStarted) {
-    //     this.allQuestions.forEach(qu => qu.questionAnswerValue = "")
-    //   }
-    // }
+  },
+  created() {
+    window.addEventListener("resize", this.setContainerHeight);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.setContainerHeight);
   },
   watch: {
     resetQuiz: function(quizStarted){
       if(this.quizStarted == false) {
         this.allQuestions.forEach(qu => qu.questionAnswerValue = "")
       }
-    }
+    },
   }
 })
